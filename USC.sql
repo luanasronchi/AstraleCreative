1dde97e5bbb3a8ecb969f61abc5d4daa4cccf86f
Luana Siqueira Ronchi <luana.sronchi@gmail.com>
2022-09-12 06:24:16 -0300

    Update SQL

--- USC.sql
+++ USC.sql
@@ -22,13 +22,31 @@ create table Pacientes(
 
 create table Procedimentos(
 	id int primary key auto_increment,
-    id_pet int,
-    id_med int,
+    id_paciente int,
+    id_medica int,
     tipo varchar(30),
     dia date,
     hora time,
     descricao varchar(1500),
+    valor float,
     status varchar(15),
-    CONSTRAINT fk_id_pet_procedimentos FOREIGN KEY (id_pet) REFERENCES Pets(id),
-    CONSTRAINT fk_id_med_procedimentos FOREIGN KEY (id_med) REFERENCES Usuarios(id)
+    CONSTRAINT fk_id_paciente_procedimentos FOREIGN KEY (id_paciente) REFERENCES Pacientes(id),
+    CONSTRAINT fk_id_medica_procedimentos FOREIGN KEY (id_medica) REFERENCES Medicas(id)
+);
+
+
+
+create table Materiais(
+    id int primary key auto_increment,
+    nome varchar(80),
+    quantidade int,
+
+);
+
+create table Fornecedores(
+    id int primary key auto_increment,
+    nome varchar(80),
+    telefone char(11),
+    id_material int,
+    CONSTRAINT fk_id_material_fornecedores FOREIGN KEY (id_material) REFERENCES Materiais(id)
 );
