eb080b34d487b2ea37d616fec205f2737dc859e1
Luana Siqueira Ronchi <lunahryy@gmail.com>
2022-09-19 12:26:36 -0300

    Update: SQL

--- USC.sql
+++ USC.sql
@@ -2,7 +2,7 @@ drop  database if exists UnicSteticCenter;
 create database UnicSteticCenter;
 use UnicSteticCenter;
 
-create table Medicas(
+create table Administradores(
 	id int primary key auto_increment,
     usuario varchar(50) unique,
     senha varchar(50)
@@ -13,7 +13,7 @@ create table Pacientes(
     nome varchar(40),
     email varchar(150),
     senha varchar(80),
-    idade int,
+    nascimento date,
     cpf char(11) unique,
     telefone char(11),
     cidade varchar(80),
@@ -22,7 +22,7 @@ create table Pacientes(
     observacao varchar(254)
 );
 
-create table Procedimentos(
+create table Consultas(
 	id int primary key auto_increment,
     id_paciente int,
     id_medica int,
@@ -32,15 +32,16 @@ create table Procedimentos(
     descricao varchar(1500),
     valor float,
     status varchar(15),
-    CONSTRAINT fk_id_paciente_procedimentos FOREIGN KEY (id_paciente) REFERENCES Pacientes(id),
-    CONSTRAINT fk_id_medica_procedimentos FOREIGN KEY (id_medica) REFERENCES Medicas(id)
+    CONSTRAINT fk_id_paciente_consultas FOREIGN KEY (id_paciente) REFERENCES Pacientes(id),
+    CONSTRAINT fk_id_medica_consultas FOREIGN KEY (id_medica) REFERENCES Administradores(id)
 );
 
 create table Produtos(
     id int primary key auto_increment,
     nome varchar(80),
     quantidade float,
-    descricao varchar(500),
+    medida varchar(30),
+    descricao varchar(500)
 );
 
 create table Fornecedores(
@@ -51,6 +52,31 @@ create table Fornecedores(
     id_produto int,
     CONSTRAINT fk_id_produto_fornecedores FOREIGN KEY (id_produto) REFERENCES Produtos(id)
 );
+
+create table Produtos_Usados(
+	id int primary key auto_increment,
+    id_produto int,
+    id_procedimento int,
+    quantidade int
+);
+
+create table Procedimentos(
+	id int primary key auto_increment,
+    id_paciente int,
+    id_medica int,  
+    tipo varchar(30),
+    dia date,
+    hora time,
+    produtos_usados int,
+    descricao varchar(1500),
+    valor float,
+    status varchar(15),
+    CONSTRAINT fk_id_paciente_procedimentos FOREIGN KEY (id_paciente) REFERENCES Pacientes(id),
+    CONSTRAINT fk_id_medica_procedimentos FOREIGN KEY (id_medica) REFERENCES Administradores(id),
+    CONSTRAINT fk_id_medica_procedimentos FOREIGN KEY (id_medica) REFERENCES (id)
+);
+
+
 
 
 
