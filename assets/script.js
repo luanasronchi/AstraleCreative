d9e520487a1c9a3a614da6437f2fe5295a4919c1
Luana Siqueira Ronchi <lunahryy@gmail.com>
2022-09-27 08:48:58 -0300

    Refactor

--- assets/script.js
+++ assets/script.js
@@ -5,14 +5,15 @@ function CheckTheme(){
 }
 
 function ChangeTheme(theme){
-    document.body.classList.remove('white_theme'); 
-    document.body.classList.remove('black_theme'); 
-
-    document.body.classList.add(theme);
     if (theme == 'white_theme'){
         document.getElementById("sidebar_logo").src = "assets/img/sidebar_logo_black.svg"
     }
     else {
         document.getElementById("sidebar_logo").src = "assets/img/sidebar_logo_white.svg"
     }
+    document.body.classList.remove('white_theme'); 
+    document.body.classList.remove('black_theme'); 
+
+    document.body.classList.add(theme);
+    
 }
