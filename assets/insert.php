ca21760d1360b4bd95bf16ca8cf5d970c43f668c
Luana Siqueira Ronchi <lunahryy@gmail.com>
2022-09-27 09:12:35 -0300

    PHP

--- assets/insert.php
+++ assets/insert.php
@@ -1,6 +1,5 @@
 <?php
     include("conecta.php");
-    $id = $_POST["id"];
     $nome = $_POST["nome"];
     $email = $_POST["email"];
     $senha = $_POST["senha"];
@@ -10,11 +9,9 @@
     $cidade = $_POST["cidade"];
     $rua = $_POST["rua"];
     $numero_casa = $_POST["numero_casa"];
-    $observacao = $_POST["observacao"];
 
    
     $comando = $pdo -> prepare("INSERT INTO Pacientes(nome, email, senha, nascimento, cpf, telefone, cidade, rua, numero_casa, observacao) VALUES(:nome, :email, :senha, :nascimento, :cpf, :telefone, :cidade, :rua, :numero_casa, :observacao)");  // Prepara o Comando de inserção
-    $comando->bindValue(":id",$id);                                           // Seleciona o dado que será inserido
     $comando->bindValue(":nome",$nome);                                         // Seleciona o dado que será inserido
     $comando->bindValue(":email",$email);                                        // Seleciona o dado que será inserido
     $comando->bindValue(":senha",$senha);                                        // Seleciona o dado que será inserido
@@ -24,7 +21,7 @@
     $comando->bindValue(":cidade",$cidade);                                       // Seleciona o dado que será inserido
     $comando->bindValue(":rua",$rua);                                          // Seleciona o dado que será inserido
     $comando->bindValue(":numero_casa",$numero_casa);                                  // Seleciona o dado que será inserido
-    $comando->bindValue(":observacao",null);                                   // Seleciona o dado que será inserido
+    $comando->bindValue(":observacao","");                                   // Seleciona o dado que será inserido
     $comando->execute();                                                        // Executa o comando
 
     echo("Saved");
