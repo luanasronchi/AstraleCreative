11941a1c9c18c6ebe86e52253db263f5f31ac27b
Luana Siqueira Ronchi <lunahryy@gmail.com>
2022-10-19 12:17:24 -0300

    Update: session

--- assets/USC.sql
+++ assets/USC.sql
@@ -2,19 +2,7 @@ drop  database if exists UnicSteticCenter;
 create database UnicSteticCenter;
 use UnicSteticCenter;
 
-create table Administradores(
-	id int primary key auto_increment,
-    usuario varchar(50) unique,
-    senha varchar(50)
-);
-
-insert into Administradores(usuario, senha) values 
-    ("lara",123),
-    ("aurora",456),
-    ("malu",789),
-    ("recepcao",246);
-
-create table Pacientes(
+create table Usuarios(
 	id int primary key auto_increment,
     nome varchar(40),
     email varchar(150),
@@ -25,9 +13,16 @@ create table Pacientes(
     cidade varchar(80),
     rua varchar(80),
     numero_casa int,
-    observacao varchar(254)
+    observacao varchar(254),
+    id_adm boolean
 );
 
+insert into Usuarios(nome, email, senha, telefone, is_adm) values 
+    ("Dr. Lara Martinelli","laram@usc.com",123,"47931033632",1),
+    ("Dr. Aurora Baker","aurorab@usc.com",456,"47924732214",1),
+    ("Dr. Malu Carter","maluc@usc.com",789,"47938848504",1),
+    ("Recepção","recepcao@usc.com",246,"47937151868",1);
+    
 create table Consultas(
 	id int primary key auto_increment,
     id_paciente int,
@@ -38,8 +33,8 @@ create table Consultas(
     habil boolean,
     valor float,
     status varchar(15),
-    CONSTRAINT fk_id_paciente_consultas FOREIGN KEY (id_paciente) REFERENCES Pacientes(id),
-    CONSTRAINT fk_id_medica_consultas FOREIGN KEY (id_medica) REFERENCES Administradores(id)
+    CONSTRAINT fk_id_paciente_consultas FOREIGN KEY (id_paciente) REFERENCES Usuarios(id),
+    CONSTRAINT fk_id_medica_consultas FOREIGN KEY (id_medica) REFERENCES Usuarios(id)
 );
 
 create table Produtos(
@@ -79,8 +74,8 @@ create table Procedimentos(
     observacao varchar(1500),
     valor float,
     status varchar(15),
-    CONSTRAINT fk_id_paciente_procedimentos FOREIGN KEY (id_paciente) REFERENCES Pacientes(id),
-    CONSTRAINT fk_id_medica_procedimentos FOREIGN KEY (id_medica) REFERENCES Administradores(id),
+    CONSTRAINT fk_id_paciente_procedimentos FOREIGN KEY (id_paciente) REFERENCES Usuarios(id),
+    CONSTRAINT fk_id_medica_procedimentos FOREIGN KEY (id_medica) REFERENCES Usuarios(id),
     CONSTRAINT fk_id_produtos_usados_procedimentos FOREIGN KEY (id_produtos_usados) REFERENCES Produtos_Usados(id)
 );
 
