8a451a0be5fbb7c298dc906adb482c2461fdefe0
Luana Siqueira Ronchi <luana.sronchi@gmail.com>
2022-11-16 21:03:40 -0300

    Update final

--- assets/USC.sql
+++ assets/USC.sql
@@ -32,29 +32,10 @@ create table Consultas(
     observacao varchar(1500),
     habil boolean,
     valor float,
-    status varchar(15),
+    status varchar(15) DEFAULT 'Pendente',
     CONSTRAINT fk_id_paciente_consultas FOREIGN KEY (id_paciente) REFERENCES Usuarios(id),
     CONSTRAINT fk_id_medica_consultas FOREIGN KEY (id_medica) REFERENCES Usuarios(id)
 );
-
-create table Produtos(
-    id int primary key auto_increment,
-    nome varchar(80),
-    quantidade float,
-    medida varchar(30),
-    descricao varchar(500)
-);
-
-create table Fornecedores(
-    id int primary key auto_increment,
-    nome varchar(80),
-    telefone char(11),
-    cpf_cnpj varchar(15),
-    id_produto int,
-    CONSTRAINT fk_id_produto_fornecedores FOREIGN KEY (id_produto) REFERENCES Produtos(id)
-);
-
-
 
 create table Procedimentos(
 	id int primary key auto_increment,
@@ -63,7 +44,6 @@ create table Procedimentos(
     id_medica int,
     dia date,
     hora time,
-    id_produtos_usados int,
     observacao varchar(1500),
     valor float,
     status varchar(15) DEFAULT 'Pendente',
--- php/insert_procedimentos.php
+++ php/insert_procedimentos.php
@@ -1,6 +1,5 @@
 <?php
     include("conecta.php");
-
     $cpf = $_POST["cpf"];
     $medica = $_POST["medica"];
     $procedimento = $_POST["procedimento"];
@@ -11,7 +10,6 @@
     $id_paciente = $pdo->query("SELECT id FROM usuarios WHERE cpf='$cpf'")->fetch();
     $id_string = $id_paciente["0"];
 
-
     $comando = $pdo->prepare("INSERT INTO Procedimentos(id_paciente, id_medica, procedimento, dia, hora, observacao) VALUES(:id_paciente, :id_medica, :procedimento, :dia, :hora, :observacao)");  // Prepara o Comando de inserção
     $comando->bindValue(":id_paciente",$id_string);
     $comando->bindValue(":id_medica",$medica);
@@ -20,12 +18,5 @@
     $comando->bindValue(":hora",$hora);
     $comando->bindValue(":observacao",$descricao);
     $comando->execute();
-    
-
-    echo $cpf = $_POST["cpf"];
-    echo $medica = $_POST["medica"];
-    echo $procedimento = $_POST["procedimento"];
-    echo $hora = $_POST["hora"];
-    echo $dia = $_POST["dia"];
-    echo $descricao = $_POST["descricao"];
+    header("Location:../agenda.php");  
 ?>
